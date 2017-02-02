cf7a62630435c15e5094bb6029cf32868a38b96f
Gustavo Lizarraga <lizarraga.gux@gmail.com>
2017-02-02 18:30:28 -0400

    Componentes de la aplicacion

---
+++ app/src/main/java/com/miramicodigo/toddler/ui/components/DialogoDetalle.java
@@ -0,0 +1,43 @@
+package com.miramicodigo.toddler.ui.components;
+
+
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.support.v4.app.DialogFragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.miramicodigo.toddler.R;
+
+public class DialogoDetalle extends DialogFragment {
+    private TextView tvTitulo;
+
+    public static DialogoDetalle newInstance(String title) {
+        DialogoDetalle frag = new DialogoDetalle();
+        Bundle args = new Bundle();
+        args.putString("title", title);
+        frag.setArguments(args);
+        return frag;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.dialog_details, container);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        tvTitulo = (TextView) view.findViewById(R.id.tv_dialog_titulo);
+        String title = getArguments().getString("title", "Enter Name");
+        getDialog().setTitle(title);
+        tvTitulo.setText("ACA MANDAMOS");
+        getDialog().getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+    }
+
+}
