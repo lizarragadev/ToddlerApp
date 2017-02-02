71802f8d569d59fcc857a515fa5b28900157adbd
Gustavo Lizarraga <lizarraga.gux@gmail.com>
2017-02-02 18:33:09 -0400

    Archivo de configuracion app

---
+++ app/src/main/java/com/miramicodigo/toddler/view/Config.java
@@ -0,0 +1,14 @@
+package com.miramicodigo.toddler.view;
+
+import android.graphics.Color;
+
+/**
+ * Created by gusn8 on 31-01-17.
+ */
+
+public final class Config {
+    public static final boolean DEBUG = Boolean.parseBoolean("true");
+    public static final int VERSION_CODE = 1;
+    public static final String VERSION_NAME = "1.0";
+    public static final String HOST = "http://inedkelly.hol.es/proyectoGrado/mobile";
+}
