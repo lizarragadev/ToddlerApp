3e909bfd893d7d34f7280fa60058c5e3f542d2d5
Gustavo Lizarraga <lizarraga.gux@gmail.com>
2017-02-02 18:37:45 -0400

    Archivo de configuracion App

--- app/src/main/java/com/miramicodigo/toddler/view/Config.java
+++ app/src/main/java/com/miramicodigo/toddler/view/Config.java
@@ -10,5 +10,5 @@ public final class Config {
     public static final boolean DEBUG = Boolean.parseBoolean("true");
     public static final int VERSION_CODE = 1;
     public static final String VERSION_NAME = "1.0";
-    public static final String HOST = "http://inedkelly.hol.es/proyectoGrado/mobile";
+    public static final String HOST = "URL";
 }
