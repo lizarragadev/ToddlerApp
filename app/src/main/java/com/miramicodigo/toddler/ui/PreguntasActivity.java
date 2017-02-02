958285c93462f0b9a533d2605ed3210dea04c513
Gustavo Lizarraga <lizarraga.gux@gmail.com>
2017-02-02 18:32:17 -0400

    Activity donde se mostraran las preguntas con descripcion de acuerdo al tipo que sea

---
+++ app/src/main/java/com/miramicodigo/toddler/ui/PreguntasActivity.java
@@ -0,0 +1,17 @@
+package com.miramicodigo.toddler.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.miramicodigo.toddler.R;
+
+public class PreguntasActivity extends BaseActivity {
+
+    private static final String TAG = "PreguntasActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_preguntas);
+    }
+}
