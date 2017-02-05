81d416f3f3eb490aebdc49c10f3b5f4c41922b95
Gustavo Lizarraga <lizarraga.gux@gmail.com>
2017-02-04 23:14:10 -0400

    Adicion del presentador para hacer click a un card

---
+++ app/src/main/java/com/miramicodigo/toddler/presenter/RecyclerItemClickListener.java
@@ -0,0 +1,9 @@
+package com.miramicodigo.toddler.presenter;
+
+/**
+ * Created by gusn8 on 03-02-17.
+ */
+
+public interface RecyclerItemClickListener {
+    void onItemClickListener(int position);
+}
