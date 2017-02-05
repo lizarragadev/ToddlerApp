02ff80e203019bf2e85f3ea2d2ef3f87dc96ee52
Gustavo Lizarraga <lizarraga.gux@gmail.com>
2017-02-04 23:19:12 -0400

    Adicion de variables globales para heredar en varias activities asi como metodos del Snackbat

--- app/src/main/java/com/miramicodigo/toddler/ui/BaseActivity.java
+++ app/src/main/java/com/miramicodigo/toddler/ui/BaseActivity.java
@@ -4,43 +4,49 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.Snackbar;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.TextView;
+
+import com.miramicodigo.toddler.R;
 
 import butterknife.ButterKnife;
 
 public class BaseActivity extends AppCompatActivity {
 
-    private static final int SUCCESS = 3;
-    private static final int WARNING = 2;
-    private static final int ERROR = 1;
+    static final int RES_DESARROLLADO = 3;
+    static final int RES_EN_PROGRESO = 2;
+    static final int RES_PREOCUPANTE = 1;
+
+    static final int MOTOR_GRUESO = 1;
+    static final int MOTOR_FINO = 2;
+    static final int LENGUAJE = 3;
+
+    static final String PREOCUPANTE = "PREOCUPANTE";
+    static final String EN_PROGRESO = "EN PROGRESO";
+    static final String DESARROLLADO = "DESARROLLADO";
 
     protected void injectViews() {
         ButterKnife.bind(this);
     }
 
     protected void showMessage(View container, String message, int type) {
-        Snackbar snackbar = Snackbar.make(container,message, Snackbar.LENGTH_LONG);
-        if (type == ERROR) {
-            snackbar.setActionTextColor(Color.RED);
+        Snackbar snackbar = Snackbar.make(container, message, 4000);
+        View sbView = snackbar.getView();
+        if (type == RES_PREOCUPANTE) {
+            sbView.setBackgroundColor(ContextCompat.getColor(getApplicationContext(), R.color.color_preocupante));
         } else {
-            if(type == 2) {
-                snackbar.setActionTextColor(Color.YELLOW);
+            if(type == RES_EN_PROGRESO) {
+                sbView.setBackgroundColor(ContextCompat.getColor(getApplicationContext(), R.color.color_enprogreso));
             }else {
-                snackbar.setActionTextColor(Color.GREEN);
+                sbView.setBackgroundColor(ContextCompat.getColor(getApplicationContext(), R.color.color_desarrollado));
             }
         }
-        View sbView = snackbar.getView();
         TextView textView = (TextView) sbView.findViewById(android.support.design.R.id.snackbar_text);
+        textView.setGravity(Gravity.CENTER_HORIZONTAL);
         textView.setTextColor(Color.WHITE);
         snackbar.show();
-    }
-
-    protected void next(Bundle bundle, Class<?> activity, boolean destroy) {
-        Intent intent= new Intent(this,activity);
-        if(bundle!=null){intent.putExtras(bundle);}
-        startActivity(intent);
-        if(destroy)finish();
     }
 }
