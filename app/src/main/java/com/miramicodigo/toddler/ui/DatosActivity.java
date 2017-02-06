418aad5c0598c5af4068106c1bef43da6fad8c20
Gustavo Lizarraga <lizarraga.gux@gmail.com>
2017-02-05 23:54:07 -0400

    Cambio en las variables para obtencion de datos de la pantalla

--- app/src/main/java/com/miramicodigo/toddler/ui/DatosActivity.java
+++ app/src/main/java/com/miramicodigo/toddler/ui/DatosActivity.java
@@ -2,6 +2,7 @@ package com.miramicodigo.toddler.ui;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.TextInputEditText;
 import android.support.v4.app.FragmentManager;
 import android.view.View;
 import android.view.WindowManager;
@@ -10,9 +11,19 @@ import android.widget.Button;
 import com.miramicodigo.toddler.R;
 import com.miramicodigo.toddler.ui.components.DialogoDetalle;
 
+import butterknife.BindView;
 import butterknife.OnClick;
 
 public class DatosActivity extends BaseActivity {
+
+    @BindView(R.id.tiet_datos_nombre)
+    TextInputEditText tietNombre;
+    @BindView(R.id.tiet_datos_ci)
+    TextInputEditText tietCI;
+    @BindView(R.id.tiet_datos_edad)
+    TextInputEditText tietEdad;
+    @BindView(R.id.tiet_datos_nombretutor)
+    TextInputEditText tietNombreTutor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,7 +44,11 @@ public class DatosActivity extends BaseActivity {
 
     @OnClick(R.id.btn_datos_siguiente)
     public void goToMain(Button button) {
-        System.out.println("HIZO CLICK BOTON");
-        startActivity(new Intent(getApplicationContext(), MenuActivity.class));
+        Intent intent = new Intent(getApplicationContext(), MenuActivity.class);
+        intent.putExtra("nombre", tietNombre.getText().toString());
+        intent.putExtra("ci", tietCI.getText().toString());
+        intent.putExtra("edad", tietEdad.getText().toString());
+        intent.putExtra("tutor", tietNombreTutor.getText().toString());
+        startActivity(intent);
     }
 }
