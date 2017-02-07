59ae66a3b53c53082e02577cf88d10b74f83be1c
Gustavo Lizarraga <lizarraga.gux@gmail.com>
2017-02-06 22:26:40 -0400

    Agregado de la animacion para el splashscreen

--- app/src/main/java/com/miramicodigo/toddler/ui/SplashscreenActivity.java
+++ app/src/main/java/com/miramicodigo/toddler/ui/SplashscreenActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.miramicodigo.toddler.R;
 
@@ -13,12 +14,15 @@ import butterknife.BindView;
 public class SplashscreenActivity extends BaseActivity {
 
     private Animation animation;
+    private Animation animation_letra;
 
     @BindView(R.id.ivLogo) ImageView ivLogo;
+    @BindView(R.id.tv_splash_titulo) TextView tvTitulo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_splashscreen);
 
         injectViews();
@@ -43,10 +47,28 @@ public class SplashscreenActivity extends BaseActivity {
 
             }
         });
+        tvTitulo.startAnimation(animation_letra);
+        animation_letra.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
     }
 
     public void ui() {
         animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.animation);
+        animation_letra = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.animation_letra);
     }
 
 }
