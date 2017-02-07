9b125f59cfe96b87084280c3e33281af0c8068e9
Gustavo Lizarraga <lizarraga.gux@gmail.com>
2017-02-06 22:23:14 -0400

    Modificacion en la pantalla principal para adicionar la animacion

--- app/src/main/java/com/miramicodigo/toddler/ui/MainActivity.java
+++ app/src/main/java/com/miramicodigo/toddler/ui/MainActivity.java
@@ -2,23 +2,38 @@ package com.miramicodigo.toddler.ui;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 import com.miramicodigo.toddler.R;
+
+import butterknife.BindView;
 import butterknife.OnClick;
 
 public class MainActivity extends BaseActivity {
 
+    @BindView(R.id.iv_main_logo)
+    ImageView imageView;
+
+    private Animation animation;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
 
+        mainActivity = this;
+
         injectViews();
+
+        animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.blink);
+        imageView.startAnimation(animation);
     }
 
     @OnClick(R.id.btnEmpezar)
     public void empezar() {
-        System.out.println("LLEGA");
         startActivity(new Intent(getApplicationContext(), DatosActivity.class));
     }
 }
